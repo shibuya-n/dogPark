9bd7e8f1f6ff1b1919fb1c7a6e5a94a185b92b2a
shibuya-n <a.duong2006@gmail.com>
2022-11-02 14:07:53 -0700

    park worky

---
+++ src/parkDog.java
@@ -0,0 +1,12 @@
+public class parkDog {
+    public int cuteness;
+    public String name;
+    public String breed;
+    public parkDog(int cuteness, String name, String breed) {
+
+        this.cuteness = cuteness;
+        this.name = name;
+        this.breed = breed;
+
+    }
+}
